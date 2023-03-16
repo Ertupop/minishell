9dd2dc32e13c1b6751450c1f7771553a358042c1
ertupop <rom-2001@hotmail.fr>
2023-03-16 09:58:51 +0100

    ajout de gestin status dans heredoc

--- srcs/parsing/use_3.c
+++ srcs/parsing/use_3.c
@@ -6,18 +6,19 @@
 /*   By: ertupop <ertupop@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 14:09:16 by jule-mer          #+#    #+#             */
-/*   Updated: 2023/03/11 11:45:52 by ertupop          ###   ########.fr       */
+/*   Updated: 2023/03/16 09:53:48 by ertupop          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	ft_launch_heredoc(char *eof, t_list **collector, int fd)
+int	ft_launch_heredoc(char *eof, t_list **collector, int fd)
 {
 	pid_t	child;
 	char	*end;
+	int		result;
 
- 	child = fork();
+	child = fork();
 	if (child == 0)
 	{
 		end = ft_strdup(eof);
@@ -26,7 +27,16 @@ void	ft_launch_heredoc(char *eof, t_list **collector, int fd)
 		free(end);
 	}
 	else
-		waitpid(child, NULL, 0);
+		waitpid(child, &result, 0);
+	if (WIFSIGNALED(result))
+	{
+		result = WTERMSIG(result) + 128;
+		if (WTERMSIG(result) == SIGQUIT)
+			write(STDERR_FILENO, "Quit (core dumped)\n", 19);
+		else if (WTERMSIG(result) == SIGSEGV)
+			write(STDERR_FILENO, "Segmentation fault (core dumped)\n", 33);
+	}
+
 }
 
 
@@ -43,7 +53,7 @@ void	ft_add_heredoc(t_use **use, t_list **collector, int limiter,
 		new->tokken = LIMITER;
 		new->tab = NULL;
 		new->fd = ft_acces_heredoc();
-		if(new->fd == -1)
+		if (new->fd == -1)
 			return ;
 		ft_lstadd_back_use(use, new);
 		while (bridge && bridge->tokken != PIPE)
