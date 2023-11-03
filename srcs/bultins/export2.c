92491c2d5b9a7f4be5031b6af3de5cd22ae935c7
Ertupop <rstrub@student.42.fr>
2023-11-03 10:49:04 +0100

    resolution message erreur export

--- srcs/bultins/export2.c
+++ srcs/bultins/export2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ertupop <ertupop@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rstrub <rstrub@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 08:20:29 by ertupop           #+#    #+#             */
-/*   Updated: 2023/01/02 09:07:07 by ertupop          ###   ########.fr       */
+/*   Updated: 2023/11/03 10:47:51 by rstrub           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ int	ft_check_export(char *str)
 	if ((str[0] < 'a' || str[0] > 'z') && (str[0] < 'A'
 			|| str[0] > 'Z') && str[0] != '_')
 	{
-		write(2, "minishell : export: `", 9);
+		write(2, "minishell : export: ", 20);
 		ft_putstr_fd(str, 2);
-		write(2, "': not a valid identifier\n", 26);
+		write(2, " : not a valid identifier\n", 26);
 		return (1);
 	}
 	i2 = 1;
@@ -41,9 +41,9 @@ int	ft_check_export(char *str)
 				|| str[i2] > 'Z') && str[i2] != '_' && (str[i2] < '0'
 				|| str[i2] > '9') && (str[i2] != '+' && str[i2 + 1] != '='))
 		{
-			write(2, "minishell : export: `", 9);
+			write(2, "minishell : export: ", 20);
 			ft_putstr_fd(str, 2);
-			write(2, "': not a valid identifier\n", 26);
+			write(2, " : not a valid identifier\n", 26);
 			return (1);
 		}
 		i2++;
